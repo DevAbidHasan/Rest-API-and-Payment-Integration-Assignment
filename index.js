92209b095b4554a31845257606729c5483810832
DevAbidHasan <abidhasanplabon80@gmail.com>
2026-01-21 15:18:57 +0600

    connection setup done with mongodb atlas

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+require('dotenv').config();
+const cors =require('cors');
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req,res)=>{
-    res.send("server is running");
-})
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcbondo.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, ()=>{
     console.log("server is running on port : ", port);
